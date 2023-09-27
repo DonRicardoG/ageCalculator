9eef55fc67873811c82db408b4379e1c48441627
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2023-09-27 11:38:28 -0600

    fixing style

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -10,11 +10,10 @@ const Main = () => {
   return (
     <div className={style.mainContainer}>
       <Form setDays={setDays} setMonths={setMonths} setYears={setYears} />
-      <div className={style.ageContainer}>
-        <h1>{years === "" ? <span>--</span> : <span>{years}</span>}years</h1>
-        <h1>{months === "" ? <span>--</span> : <span>{months}</span>}months</h1>
-        <h1>{days === "" ? <span>--</span> : <span>{days}</span>}days</h1>
-      </div>
+
+      <h1>{years === "" ? <span>--</span> : <span>{years}</span>}years</h1>
+      <h1>{months === "" ? <span>--</span> : <span>{months}</span>}months</h1>
+      <h1>{days === "" ? <span>--</span> : <span>{days}</span>}days</h1>
     </div>
   );
 };
